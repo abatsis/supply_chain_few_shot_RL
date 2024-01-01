61a5df12fb8b32274bf04d6aaaaed039c9ef7c00
abatsis <abatsis@mail.com>
2024-01-01 18:56:50 +0000

    Add skewed data generation functionality

--- src/data/generate.py
+++ src/data/generate.py
@@ -1,3 +1,5 @@
+import argparse
+
 import tqdm
 import or_gym
 from or_gym.utils import create_env
@@ -8,12 +10,15 @@ import os
 import secrets
 import numpy as np
 import torch as th
-import sys
 from stable_baselines3.common.monitor import Monitor
 
 th.set_num_threads(1)
 
-
+parser = argparse.ArgumentParser()
+parser.add_argument('data_dir')
+parser.add_argument('--skewed', action='store_true')
+args = parser.parse_args()
+args = vars(args)
 
 def poison_rand():
     return {'mu': np.random.uniform(10, 100)}
@@ -40,6 +45,24 @@ switch = {
 
 
 def create_random_env_config(distribution_choice, m):
+    return dict(
+        periods=np.random.randint(10, 100),
+        I0=[np.random.randint(10, 100) for i in range(m - 1)],
+        p=np.random.randint(2, 100),
+        r=[np.random.random() * 2 for i in range(m)],
+        k=[np.random.random() for i in range(m)],
+        h=[np.random.random() * 0.5 for i in range(m - 1)],
+        c=[np.random.randint(10, 100) for i in range(m - 1)],
+        L=[np.random.randint(1, 20) for i in range(m - 1)],
+        backlog=False,
+        dist=distribution_choice,
+        dist_param=switch[distribution_choice],
+        alpha=0.97,
+        seed_int=0,
+    )
+
+
+def create_random_env_config_skewed(distribution_choice, m):
     return dict(
         periods=np.random.randint(10, 100),
         I0=[np.random.randint(10, 100) for i in range(m - 1)],
@@ -86,7 +109,8 @@ env_name = 'InvManagement-v1'
 m = np.random.randint(3, 10)
 # d = np.random.choice([1,2,3,4])
 distribution_choice = 3
-env_config = create_random_env_config(distribution_choice, m)
+env_config_generator = create_random_env_config_skewed if args['skewed'] else create_random_env_config
+env_config = env_config_generator(distribution_choice, m)
 
 print(env_config)
 
@@ -104,7 +128,7 @@ for i in pbar:
 
 obs, actions, rewards = evaluate_using_checkpoint(model, 20, env)
 
-data_dir = sys.argv[1]
+data_dir = args['data_dir']
 os.makedirs(data_dir, exist_ok=True)
 file = data_dir + "/" + secrets.token_hex(nbytes=32) + ".npz"
 np.savez(file, obs=obs, actions=actions, rewards=rewards, config=env_config)
