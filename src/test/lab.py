c5d317ca5b315178853dbfd00e2b6f4ef9d5955e
abatsis <abatsis@mail.com>
2024-01-15 23:38:14 +0000

    Account for poison dist in env configs

--- src/test/lab.py
+++ src/test/lab.py
@@ -38,8 +38,10 @@ class Lab:
             input = np.array([input])
         elif isinstance(input, bool):
             input = np.array([float[input]])
-        elif isinstance(input, dict):
+        elif isinstance(input, dict) and 'low' in input:
             input = np.array([input['low'], input['high']])
+        elif isinstance(input, dict):
+            input = np.array([input['mu'], 100])
         elif isinstance(input, float):
             input = np.array([input])
         return input
